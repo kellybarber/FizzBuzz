fc50e08705b951c177105ddea9a8240345f476e9
kellybarber <kelly.james.barber@gmail.com>
2018-01-12 18:38:04 -0500

    first test passing

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,13 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by Kelly Barber on 2018-01-12.
+//  Copyright © 2018 Kelly Barber. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+    
+}
