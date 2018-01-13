8987445d99a305881960fcc416531058e558223f
kellybarber <kelly.james.barber@gmail.com>
2018-01-13 17:43:51 -0500

    created new file for testing the game engine

---
+++ FizzBuzzTests/GameTests.swift
@@ -0,0 +1,25 @@
+//
+//  GameTests.swift
+//  FizzBuzzTests
+//
+//  Created by Kelly Barber on 2018-01-13.
+//  Copyright © 2018 Kelly Barber. All rights reserved.
+//
+
+import XCTest
+
+class GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    
+}
