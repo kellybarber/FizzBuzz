08c76a546971b0fb1202f7869706612bda2fbb4d
kellybarber <kelly.james.barber@gmail.com>
2018-01-13 17:53:46 -0500

    test for game score starting at 0 is passing

---
+++ FizzBuzz/Game.swift
@@ -0,0 +1,19 @@
+//
+//  Game.swift
+//  FizzBuzz
+//
+//  Created by Kelly Barber on 2018-01-13.
+//  Copyright © 2018 Kelly Barber. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    
+    var score: Int
+    
+    init() {
+        score = 0
+    }
+    
+}
