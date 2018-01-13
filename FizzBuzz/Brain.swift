04423ada2fed8447031cafaee60cb046bced279a
kellybarber <kelly.james.barber@gmail.com>
2018-01-13 17:15:21 -0500

    fizzbuzz test passing

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -27,7 +27,9 @@ class Brain {
     }
     
     func check(number: Int) -> String {
-        if number % 3 == 0 {
+        if number % 15 == 0 {
+            return "FizzBuzz"
+        } else if number % 3 == 0 {
             return "Fizz"
         } else {
             return "Buzz"
